fb15a7038687244c0843e3cabdda8ddb80b14d3e
Mohamed Elhawary <69651552+Mohamed-Elhawary@users.noreply.github.com>
2025-05-23 16:05:21 +0000

    Install Highlight Packge to customize code blocks color

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Procore-Coding-Interview-Problem-Solving
-Created with CodeSandbox
+# Procore Coding Interview - Problem-Solving
+
+![Image](https://github.com/user-attachments/assets/8512a0bc-36a8-4ecc-9c89-87a176216bb4)
--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,16 @@
 import "./styles.css";
+// index.mjs
+
+import hljs from "highlight.js/lib/core";
+// Import just the languages you need for a smaller bundle:
+import javascript from "highlight.js/lib/languages/javascript";
+// Import your preferred theme:
+import "highlight.js/styles/github-dark.css";
+
+// Register the language(s)
+hljs.registerLanguage("javascript", javascript);
+
+// Highlight all code blocks
+document.addEventListener("DOMContentLoaded", () => {
+  hljs.highlightAll();
+});
